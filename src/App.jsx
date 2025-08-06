b4a9e1e7bf3c456ec945d0870e1882bc7bcb0fb3
wisdom chainx <wisdomchainx111@gmail.com>
2025-08-06 13:42:03 +0100

    fix case

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from "react"
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Home from "./Home"
-import Signup from "./SignUp"
+import Signup from "./Signup"
 import Payment from "./Payment"
 import Confirm from "./Confirm"
 
